b92d51a17e5570359c1a4e213ccf2ab3cc8f8fa7
Alfonso Landeros <alanderos@ucla.edu>
2021-02-24 09:37:25 -0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/COVID19SchoolReopening.jl
@@ -0,0 +1,5 @@
+module COVID19SchoolReopening
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using COVID19SchoolReopening
+using Test
+
+@testset "COVID19SchoolReopening.jl" begin
+    # Write your tests here.
+end
